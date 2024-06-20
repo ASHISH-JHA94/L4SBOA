9ad32a7437f001c42ae4e48b17eed60e11f6940f
kolawolecreates <131333843+kolawolecreates@users.noreply.github.com>
2024-06-20 19:39:36 +0100

    Create index.php

---
+++ L4STestKit/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>L4S Test Tool</title>
+        <link href="/css/bootstrap.min.css" rel="stylesheet">
+        <script src="js/"></script>
+    </head>
+    <body>
+        <div class="">
+            
+        </div>
+    </body>
+</html>

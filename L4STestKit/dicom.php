84c4360c6cfa06cfb6ec7ed7c6c6bffbc8836244
kolawolecreates <131333843+kolawolecreates@users.noreply.github.com>
2024-06-20 20:10:29 +0100

    Add files via upload

--- L4STestKit/dicom.php
+++ L4STestKit/dicom.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>L4S Test Tool</title>
+        <title>L4S Test Tool / DICOM</title>
         <link href="/css/bootstrap.min.css" rel="stylesheet">
         <link src="js/bootstrap.min.js">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -57,7 +57,7 @@
                             <a class="nav-link" aria-current="page" href="wearables.php">Wearables</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="dicom.php">Dicom</a>
+                            <a class="nav-link active" href="dicom.php">Dicom</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="consultation.php">Consultation</a>
--- L4STestKit/wearables.php
+++ L4STestKit/wearables.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>L4S Test Tool</title>
+        <title>L4S Test Tool / Wearables</title>
         <link href="/css/bootstrap.min.css" rel="stylesheet">
         <link src="js/bootstrap.min.js">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -54,7 +54,7 @@
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav me-auto mb-2 mb-md-0">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="wearables.php">Wearables</a>
+                            <a class="nav-link active " aria-current="page" href="wearables.php">Wearables</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="dicom.php">Dicom</a>
